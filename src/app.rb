66f239c06c27323c9674a1463994e0c5fc9bcfc5
Jaya Wijono <jayzzwijono@yahoo.com>
2022-02-04 10:56:53 +1100

    :pear: @z3n-ryanwise setting up grids for the game

--- src/app.rb
+++ src/app.rb
@@ -1,12 +1,33 @@
 require 'ruby2d'
 
-set title: "Triangle"
+set title: "Game of life"
+set background: "white"
 
-Triangle.new(
-  x1: 320, y1: 50,
-  x2: 540, y2: 430,
-  x3:100, y3: 430,
-  color: ['red','green','blue']
-)
+CELL_SIZE = 60
+WINDOW_WIDTH = CELL_SIZE * 20
+WINDOW_HEIGHT = CELL_SIZE * 20
+
+set width: WINDOW_WIDTH
+set height: WINDOW_HEIGHT
+
+# Draw the vertical lines for the grid
+(WINDOW_WIDTH/CELL_SIZE).times do |n|
+  Line.new(
+    x1: n * CELL_SIZE, y1: 0,
+    x2: n * CELL_SIZE, y2: WINDOW_HEIGHT,
+    width: 1,
+    color: 'black',
+  )
+end
+
+# Draw the horizontal lines for the grid
+(WINDOW_WIDTH/CELL_SIZE).times do |n|
+  Line.new(
+    x1: 0, y1: n * CELL_SIZE,
+    x2: WINDOW_WIDTH, y2: n * CELL_SIZE,
+    width: 1,
+    color: 'black',
+  )
+end
 
 show
