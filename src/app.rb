c2a3a7f8d2862c86bccf899a5db3780e3fa7202f
Jaya Wijono <jayzzwijono@yahoo.com>
2022-02-04 12:53:08 +1100

    wip looping

--- src/app.rb
+++ src/app.rb
@@ -32,15 +32,61 @@ end
 
 cell_state = {}
 
+tick = 0
+update do
+  if tick % 120 == 0
+    cell_state.compact!
+    p "cell_state"
+    p cell_state
+
+    cell_state.values.each do |shape|
+      p "shape"
+      p shape
+      # if drawn_cell
+        shape.remove
+        cell_state["#{shape.x},#{shape.y}"] = nil
+
+        new_shape = Square.new(
+          x: shape.x + CELL_SIZE, y: shape.y,
+          size: CELL_SIZE,
+          color: 'blue',
+          z: -10
+        )
+        cell_state["#{shape.x + CELL_SIZE},#{shape.y}"] = new_shape
+
+        new_shape = Square.new(
+          x: shape.x, y: shape.y + CELL_SIZE,
+          size: CELL_SIZE,
+          color: 'blue',
+          z: -10
+        )
+        cell_state["#{shape.x},#{shape.y + CELL_SIZE}"] = new_shape
+      # end
+    end
+  end
+  tick += 1
+end
+
+
 on :mouse_down do |event|
   vector_x = event.x - (event.x % CELL_SIZE)
   vector_y = event.y - (event.y % CELL_SIZE)
 
+  p "MOUSE DOWN EVENT!"
+  p vector_x, vector_y
+
   drawn_cell = cell_state["#{vector_x},#{vector_y}"]
 
+  p "drawn cell exist?: #{!drawn_cell.nil?}"
+
   if drawn_cell
+    p "removing drawn cell"
     drawn_cell.remove
     cell_state["#{vector_x},#{vector_y}"] = nil
+    cell_state.compact!
+    
+    p "#### cell_state after deletion"
+    p cell_state
   else
     new_shape = Square.new(
       x: vector_x, y:vector_y,
