203f7dd44ad56d676f3bc5b705a1e162e86f044a
Ryan Wise <89052310+z3n-ryanwise@users.noreply.github.com>
2022-02-07 09:38:02 +1100

    :pair: Options for game of life rule resolution

--- src/app.rb
+++ src/app.rb
@@ -1,7 +1,7 @@
 require 'ruby2d'
 
-set title: "Game of life"
-set background: "white"
+set title: 'Game of life'
+set background: 'white'
 
 CELL_SIZE = 60
 CELL_STATE = {}
@@ -12,65 +12,79 @@ set width: WINDOW_WIDTH
 set height: WINDOW_HEIGHT
 
 # Draw the vertical lines for the grid
-(WINDOW_WIDTH/CELL_SIZE).times do |n|
+(WINDOW_WIDTH / CELL_SIZE).times do |n|
   Line.new(
     x1: n * CELL_SIZE, y1: 0,
     x2: n * CELL_SIZE, y2: WINDOW_HEIGHT,
     width: 1,
-    color: 'black',
+    color: 'black'
   )
 end
 
 # Draw the horizontal lines for the grid
-(WINDOW_WIDTH/CELL_SIZE).times do |n|
+(WINDOW_WIDTH / CELL_SIZE).times do |n|
   Line.new(
     x1: 0, y1: n * CELL_SIZE,
     x2: WINDOW_WIDTH, y2: n * CELL_SIZE,
     width: 1,
-    color: 'black',
+    color: 'black'
   )
 end
 
 def render_cell(x, y)
   return if CELL_STATE["#{x},#{y}"].is_a?(Square)
 
-  new_shape = Square.new(
+  Square.new(
     x: x, y: y,
     size: CELL_SIZE,
     color: 'blue',
     z: -10
   )
-  CELL_STATE["#{x},#{y}"] = new_shape
 end
-
 
 tick = 0
 update do
-  if tick % 120 == 0
+  if tick % 120.zero?
     CELL_STATE.compact!
-    p "cell_state"
+    p 'cell_state'
     p CELL_STATE
 
-    CELL_STATE.values.each do |shape|
-      p "shape"
+    CELL_STATE.each_value do |shape|
+      p 'shape'
       p shape
-      # if drawn_cell
-        shape.remove
-        CELL_STATE["#{shape.x},#{shape.y}"] = nil
-        render_cell(shape.x+CELL_SIZE, shape.y)
-        render_cell(shape.x, shape.y+CELL_SIZE)
+      shape.remove
+      CELL_STATE["#{shape.x},#{shape.y}"] = nil
 
+      CELL_STATE["#{x},#{y}"] = render_cell(shape.x + CELL_SIZE, shape.y)
+      CELL_STATE["#{x},#{y}"] = render_cell(shape.x, shape.y + CELL_SIZE)
+
+      # current_state = CELL_STATE
+      # {
+      #   "60,60" => shape object
+      # }
+
+      # # translate state frm pixel coordinate to index coordinate
+      # Option 1: { "60,60" => shape object } =>
+      # * Option 2: [
+      #   [0,0,0,0],
+      #   [0,1,0,0],
+      #   [0,0,0,0],
+      #   [0,0,0,0]]
+
+      # new_state = GameOfLife.call(current_state, grid_width, grid_height)
+      # * Option 2: [[0,0,0,0], [0,1,0,0], [0,0,0,0], [0,0,0,0]]
+
+      # some logic to add or remove based on the diff between current_state and new_state
     end
   end
   tick += 1
 end
 
-
 on :mouse_down do |event|
   vector_x = event.x - (event.x % CELL_SIZE)
   vector_y = event.y - (event.y % CELL_SIZE)
 
-  p "MOUSE DOWN EVENT!"
+  p 'MOUSE DOWN EVENT!'
   p vector_x, vector_y
 
   drawn_cell = CELL_STATE["#{vector_x},#{vector_y}"]
@@ -78,16 +92,16 @@ on :mouse_down do |event|
   p "drawn cell exist?: #{!drawn_cell.nil?}"
 
   if drawn_cell
-    p "removing drawn cell"
+    p 'removing drawn cell'
     drawn_cell.remove
     CELL_STATE["#{vector_x},#{vector_y}"] = nil
     CELL_STATE.compact!
-    
-    p "#### cell_state after deletion"
+
+    p '#### cell_state after deletion'
     p CELL_STATE
   else
     new_shape = Square.new(
-      x: vector_x, y:vector_y,
+      x: vector_x, y: vector_y,
       size: CELL_SIZE,
       color: 'blue',
       z: 10
---
+++ src/game_of_life.rb
@@ -0,0 +1,5 @@
+class GameOfLife
+  def self.call(_input_state, grid_width:, grid_height:)
+    []
+  end
+end
