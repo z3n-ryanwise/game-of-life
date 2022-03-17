6f8fa38a183d752b5b0476f5b300f8ff3b08b9b2
Ryan Wise <89052310+z3n-ryanwise@users.noreply.github.com>
2022-03-17 18:02:27 +1100

    implement rule 4

---
+++ spec/game_of_life/rule4_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require_relative '../../src/game_of_life/rule4'
+
+describe GameOfLife::Rule4 do
+  context 'Local reproduction' do
+    let(:state) do
+      [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 0, 1, 0]
+      ]
+    end
+    let(:ex) do
+      [
+        [[0, 0], [0, 1]],
+        [[1, 0], [1, 1]]
+      ]
+    end
+
+    let(:expected_alive_cells) { [[1, 2], [1, 3], [2, 2], [2, 3], [3, 1], [3, 2], [4, 2], [4, 3]] }
+
+    it 'returns true for any expected alive cells' do
+      expected_alive_cells.each do |cell|
+        expect(described_class.rule4(state, cell)).to eq(true)
+      end
+    end
+
+    it 'returns false for other cells' do
+      state.each_with_index do |row, row_position|
+        row.each_with_index do |_col, col_position|
+          cell_coordinate = [row_position, col_position]
+          unless expected_alive_cells.include?(cell_coordinate)
+            expect(described_class.rule4(state, cell_coordinate)).to eq(false)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ src/game_of_life/rule4.rb
@@ -0,0 +1,30 @@
+module GameOfLife
+  class Rule4
+    def self.rule4(input_state, coord)
+      row_position = coord[0]
+      col_position = coord[1]
+
+      return true if input_state[row_position][col_position] == 1
+
+      width = input_state[col_position].size
+      height = input_state.size
+
+      left_neighbour = input_state[row_position][col_position - 1]
+      right_neighbour = input_state[row_position][(col_position + 1) % width]
+
+      up_neighbour = input_state[row_position - 1][col_position]
+      down_neighbour = input_state[(row_position + 1) % height][col_position]
+
+      up_left_neighbour = input_state[row_position - 1][col_position - 1]
+      up_right_neighbour = input_state[row_position - 1][(col_position + 1) % width]
+
+      down_left_neighbour = input_state[(row_position + 1) % height][col_position - 1]
+      down_right_neighbour = input_state[(row_position + 1) % height][(col_position + 1) % width]
+
+      neighbour_count = [left_neighbour, right_neighbour, up_neighbour, down_neighbour, up_left_neighbour,
+                         up_right_neighbour, down_left_neighbour, down_right_neighbour].inject(&:+)
+
+      neighbour_count == 3
+    end
+  end
+end
