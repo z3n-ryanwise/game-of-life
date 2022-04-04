05ecb579aa575d5a8c12e775636cf28824af1e87
Ryan Wise <89052310+z3n-ryanwise@users.noreply.github.com>
2022-04-04 11:44:32 +1000

    Analyse Rules business logic

--- src/game_of_life/main.rb
+++ src/game_of_life/main.rb
@@ -1,34 +1,61 @@
 module GameOfLife
+  require_relative './rule1'
+  require_relative './rule2'
+  require_relative './rule3'
+  require_relative './rule4'
+
+  NOOP_STATE = 0
+  DEAD_STATE = -1
+  ALIVE_STATE = 1
+
   class Main
     DEFAULT_RULES = [
-      GameOfLife::Rule1
-      GameOfLife::Rule2
-      GameOfLife::Rule3
+      GameOfLife::Rule1,
+      GameOfLife::Rule2,
+      GameOfLife::Rule3,
       GameOfLife::Rule4
-      
     ]
-    def self.call(input_state, rules=DEFAULT_RULES)
-      
-      # state = input_state.rule1
-      # state = state.rule2
-      # state = state.rule3
-      # state = state.rule4
+    def self.call(input_state, rules = DEFAULT_RULES)
+      alive_count = input_state.map { |row| row.map { |_| 0 } }
 
-      input_state.map do |row|
-        row.map do |cell|
-          rules[0].call(cell) ? 1 : 0
+      rules.each do |rule|
+        input_state.each_with_index.map do |row, row_position|
+          row.each_with_index.map do |_cell, col_position|
+            rule_eval = rule.call(input_state, [row_position, col_position])
+
+            case rule_eval
+            when DEAD_STATE
+              alive_count[row_position][col_position] -= 1
+            when ALIVE_STATE
+              alive_count[row_position][col_position] += 1
+            end
+          end
+        end
+        # puts '---------'
+        # puts "After Rule Eval #{rule}"
+        # puts 'input_state'
+        # pp input_state
+        # puts 'alive_count'
+        # pp alive_count
+        # puts '---------'
+      end
+
+      # pp alive_count
+
+      input_state.each_with_index.map do |row, row_position|
+        row.each_with_index.map do |_cell, col_position|
+          if alive_count[row_position][col_position].negative?
+            0
+          elsif alive_count[row_position][col_position].positive?
+            1
+          else
+            input_state[row_position][col_position]
+          end
         end
       end
 
-      state 
+      # p 'result'
+      # pp result
     end
   end
 end
-
-
-GameOfLife::Main.call(state State, [GameOfLife::Rule1, GameOfLife::Rule2] []{call: boolean})
-
-
-
-lambda.call
-GameOfLife::Rule1.calli
